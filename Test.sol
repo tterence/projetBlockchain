31da64106a89a62eaf998079c9d37cc3e5f5496f
tterence <terence.tadjifoue@gmail.com>
2017-11-06 20:12:44 +0100

    1ere version fonctionnelle + ajout getter adresses

--- Test.sol
+++ Test.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.11;
 contract Test{
     struct Demand{
-        address dest;
+        address dest; //destinataire
         uint test;
     }
     struct CV{
-        Demand[] list;
+        Demand[] list; //liste de demandes validées
     }
     mapping(address => CV) listCV;
-    address public exp;
+    address exp; //adresse de l'expéditeur
     mapping(address => Demand) demands;
     enum State {
         NONE,
@@ -20,36 +20,44 @@ contract Test{
     State public state;
     struct Response{
         Demand demand;
-        address exp;
+        address exp; //adresse du demandeur
         State state;
     }
     mapping(address => Response) responses;
+    //array d'utilisateur
+    address[] userlist;
     function Test(){
         exp = msg.sender;
+        userlist = [
+            0xca35b7d915458ef540ade6068dfe2f44e8fa733c,
+            0x14723a09acff6d2a60dcdf7aa4aff308fddc160c,
+            0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db,
+            0x583031d1113ad414f02576bd6afabfb302140225,
+            0xdd870fa1b7c4700f2bd7f44238821c26f7392148
+        ];
     }
     function sendDemand(address _dest, uint _test){
         if (msg.sender != exp) return;
         state = State.PENDING;
         demands[exp].dest = _dest;
         demands[exp].test = _test;
-        responses[_dest].demand = demands[exp];
+        responses[_dest].demand = Demand(_dest,_test);
         responses[_dest].exp = exp;
         responses[_dest].state = state;
     }
-    function sendResponse(address _dest, State _state)
-    returns(uint){
+    function sendResponse(State _state){
         //if (msg.sender == _dest) return;
         //if (responses[_dest].state == State.NONE) return;
         //or if (state == State.NONE) return;
+        address _exp = msg.sender;
         if (_state == State.ACCEPTED){
             state = State.ACCEPTED;
-            responses[_dest].state = state;
-            listCV[_dest].list.push(responses[_dest].demand);
+            responses[_exp].state = state;
+            listCV[responses[_exp].exp].list.push(responses[_exp].demand);
         }
         else{
             state = State.REJECTED;
         }
-        return listCV[_dest].list[0].test;
     }
     function getStatus(address add)constant returns(uint[]){
         Demand[] truc = listCV[add].list;
@@ -58,5 +66,8 @@ contract Test{
             ret.push(truc[i].test);
         }
         return ret;
+    }
+    function getAddresses()constant returns(address[]){
+       return userlist;
     }
 }
